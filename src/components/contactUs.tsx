74737c7e7c7863b55d5bbb2e44f4f3bd67037c8d
MightyGiant21 <tosha.hashimov@googlemail.com>
2022-10-05 20:27:35 +0100

    Frontend complete

--- src/components/contactUs.tsx
+++ src/components/contactUs.tsx
@@ -14,14 +14,18 @@ const ContactUs: React.FC<ContactUsProps> = () => {
             <div className="formContainer">
                 <h1>Contact us using the form below</h1>
                 <ContactForm />
-                <button onClick={() => {handleClick()}}>Submit</button>
             </div>
             <div className="contactInfoContainer">
                 <h1>Contact Information</h1>
-                <p>Based in North Hampshire, working nationally</p>
-                <p>Email us on info@thweld.co.uk</p>
-                <p>We are open Monday - Friday 0700 - 1700</p>
-                <p>Out of hours contact: 07927406359</p>
+                <div className="paragraphContainer">
+                    <p>Based in North Hampshire, working nationally</p>
+                    <p>Email us on info@thweld.co.uk</p>
+                    <p>We are open Monday - Friday 0700 - 1700</p>
+                    <p>Out of hours contact: 07927406359</p>
+                </div>
+            </div>
+            <div className="btnContainer">
+                <button className='submitBtn' onClick={() => {handleClick()}}>Submit</button>
             </div>
         </div>
       );
